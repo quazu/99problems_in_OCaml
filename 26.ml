5889aab4340dd25ce2372bb6a5e85f7d1d17d8d9
myname <toedge@gmail.com>
2016-02-02 17:42:28 +0900

    * Add 26th problem.
      Generate the combinations of K distinct objects chosen from the N
      elements of a list.

--- 26.ml
+++ 26.ml
@@ -3,21 +3,15 @@
   the N elements of a list.
 *)
 let extract k xs =
-  let add xs y =
-    List.rev (y :: (List.rev xs))
+  let rec aux k acc emit = function
+    | [] -> acc
+    | h :: t ->
+      if k = 1
+        then aux k (emit [h] acc) emit t
+      else
+        let new_emit x = emit (h :: x) in
+        aux k (aux (k - 1) acc new_emit t) emit t
   in
-  let deliver xs ys = 
-    List.map (add xs) ys
-  in
-  let concat xs ys = 
-    List.rev (List.rev_append ys (List.rev xs))
-  in
-  let rec loop k0 k acc tmp xs0 xs =
-    match xs with
-      | [] -> acc
-      | hd :: tl when k = 1 ->
-        loop k0 k0 (concat (deliver tmp xs) acc) [] (List.tl xs0) (List.tl xs0)
-      | hd :: tl ->
-        loop k0 (k - 1) acc (add tmp hd) xs0 tl
-  in
-    loop k k [] [] xs xs
+  let emit x acc = x :: acc in
+    aux k [] emit xs
+
