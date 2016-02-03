1f3c951596c353f997e5e7e46dcddd684cc007b2
myname <toedge@gmail.com>
2016-02-03 17:27:47 +0900

    * Generalization the sort function.
      Define quick sort as higher oeder function.

--- 28.ml
+++ 28.ml
@@ -1,14 +1,15 @@
 (*
   Sorting a list of lists according to length oth sublists.
 *)
+(* Define quick sort not to use List.sort *)
+let rec qsort f = function
+  | [] -> []
+  | x :: xs ->
+    let smaller, larger = List.partition (fun y -> f y x) xs in
+    qsort f smaller @ (x :: qsort f larger)
 
 (*
   Sort The elements of the list according to their length.
 *)
-let rec length_qsort = function
-  | [] -> []
-  | x :: xs ->
-    let smaller, larger =
-      List.partition (fun y -> List.length y < List.length x) xs
-    in
-    length_qsort smaller @ (x::length_qsort larger)
+let length_sort list =
+  qsort (fun x y -> List.length x < List.length y) list
