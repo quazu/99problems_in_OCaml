52bba43aadc7f12b7903ededb6be54bcfd68c088
myname <toedge@gmail.com>
2016-02-04 14:44:49 +0900

    * Answer 28 th problem.
      Sort by length frequency.

--- 28.ml
+++ 28.ml
@@ -13,3 +13,16 @@ let rec qsort f = function
 *)
 let length_sort list =
   qsort (fun x y -> List.length x < List.length y) list
+
+(*
+  Sort The elements of the list according to their length frequency.
+*)
+let frequency_sort list =
+  let rec freq_of e count = function
+    | [] -> count
+    | h :: tl when List.length e = List.length h ->
+      freq_of e (count + 1) tl
+    | h :: tl -> 
+      freq_of e count tl
+  in
+  qsort (fun x y -> freq_of x 0 list < freq_of y 0 list) list 
