c5d858cb0bf298d4b87ede96a5581a8bf1b95396
myname <toedge@gmail.com>
2016-03-07 18:03:06 +0900

    * Answer 42th problem in another way.

--- 42.ml
+++ 42.ml
@@ -11,3 +11,30 @@ let rec gray n =
     then ["0"; "1"]
   else (List.map (append '0') (gray (n - 1))) @
        (List.map (append '1') (gray (n - 1)))
+
+
+(*
+  Another gray code.
+*)
+
+(* Encompass lists *)
+let encompass xs ys =
+  let rec loop xs acc = function
+    | [] -> acc
+    | h :: t -> loop xs (List.map ((^) h) xs :: acc) t
+  in
+  let rec flatten = function
+    | [] -> []
+    | l::r -> l @ flatten r
+  in
+  loop xs [] ys
+    |> flatten
+
+let gray n =
+  let bits = ["0"; "1"] in
+  let rec loop acc n =
+    if n = 1
+      then acc
+    else loop (encompass bits acc) (n - 1)
+  in
+  loop bits n
